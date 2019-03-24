ee1f5584b9d6252bc3008e30b60d9a608f65a991
MoonCheung <Salvador23@163.com>
2019-03-24 16:46:48 +0800

    update config

--- nuxt.config.js
+++ nuxt.config.js
@@ -28,7 +28,8 @@ module.exports = {
   ** Global CSS
   */
   css: [
-    'element-ui/lib/theme-chalk/index.css'
+    'element-ui/lib/theme-chalk/index.css',
+    // '@/assets/styles/main.scss'
   ],
 
   /*
@@ -57,12 +58,12 @@ module.exports = {
   */
   build: {
     transpile: [/^element-ui/],
-    
+
     /*
     ** You can extend webpack config here
     */
     extend(config, ctx) {
-      
+
     }
   }
 }
