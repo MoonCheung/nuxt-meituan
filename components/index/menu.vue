1b9c0e4373467f97fc1df6e414ce47a5f5dca7ba
MoonCheung <Salvador23@163.com>
2019-04-05 23:59:26 +0800

    update menu组件

---
+++ components/index/menu.vue
@@ -0,0 +1,60 @@
+<template>
+  <div class="m-menu">
+    <dl class="nav">
+      <dt>全部分类</dt>
+      <dd></dd>
+    </dl>
+    <div class="detail">
+
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      kind: '',
+      menu: [
+        {
+          type: 'food',
+          name: '美食',
+          child: [
+            {
+              title: '美食',
+              child: ['代金券', '甜点饮品', '火锅', '自助餐', '小吃快餐']
+            }
+          ]
+        },
+        {
+          type: 'takeout',
+          name: '外卖',
+          child: [
+            {
+              title: '外卖',
+              child: ['美团外卖']
+            }
+          ]
+        },
+        {
+          type: 'hotel',
+          name: '酒店',
+          child: [
+            {
+              title: '酒店星级',
+              child: ['经济型', '舒适/三星', '高档/四星', '豪华/五星']
+            }
+          ]
+        }
+      ]
+    }
+  },
+  //计算属性被混入vue实例
+  computed: {},
+  //方法被混入vue实例
+  methods: {}
+}
+</script>
+
+<style>
+</style>
--- pages/index.vue
+++ pages/index.vue
@@ -1,10 +1,31 @@
 <template>
-
+  <div class="page-index">
+    <el-row>
+      <el-col :span="5">
+        <emenu />
+      </el-col>
+      <el-col :span="19">
+        2
+      </el-col>
+    </el-row>
+    <el-row>
+      <el-col :span="24">
+        3
+      </el-col>
+    </el-row>
+  </div>
 </template>
 
 <script>
-export default {}
+import Emenu from '@/components/index/menu'
+
+export default {
+  components: {
+    Emenu
+  }
+}
 </script>
 
-<style>
+<style  lang="scss">
+  @import "@/assets/css/index/index.scss";
 </style>
