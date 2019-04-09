ccb87840daa37ff8092f81565db8f84ce7c96fa7
MoonCheung <Salvador23@163.com>
2019-04-09 22:28:01 +0800

    commit server folder

--- server/index.js
+++ server/index.js
@@ -1,12 +1,39 @@
 import Koa from 'koa'
-
 const consola = require('consola')
 const {
   Nuxt,
   Builder
 } = require('nuxt')
+//人工配置
+import mongoose from 'mongoose'
+import bodyParser from 'koa-bodyparser'
+import session from 'koa-generic-session'
+import Redis from 'koa-redis'
+import json from 'koa-json'
+import dbConfig from './dbs/config'
+import passport from './interface/utils/passport'
+import users from './interface/users'
 
 const app = new Koa()
+//人工配置
+app.keys = ['mt', 'keyskeys']
+app.proxy = true
+app.use(session({
+  key: 'mt',
+  prefix: 'mt:uid',
+  store: new Redis()
+}))
+app.use(bodyParser({
+  extendTypes: ['json', 'form', 'text']
+}))
+app.use(json())
+
+mongoose.connect(dbConfig.dbs, {
+  useNewUrlParser: true
+})
+
+app.use(passport.initialize())
+app.use(passport.session())
 
 // Import and Set Nuxt.js options
 const config = require('../nuxt.config.js')
@@ -27,7 +54,8 @@ async function start() {
   } else {
     await nuxt.ready()
   }
-
+  //人工配置
+  app.use(users.routes()).use(users.allowedMethods())
   app.use(ctx => {
     ctx.status = 200
     ctx.respond = false // Bypass Koa's built-in response handling
--- server/interface/utils/passport.js
+++ server/interface/utils/passport.js
@@ -3,11 +3,11 @@ import LocalStrategy from 'passport-local'
 import UserModel from '../../dbs/models/users'
 
 passport.use(
-  new LocalStrategy(function (username, password, done) {
+  new LocalStrategy(async function (username, password, done) {
     let where = {
       username
     };
-    let result = await UserModel.findOne(where);
+    let result = await UserModel.findOne(where)
     if (result != null) {
       if (result.password === password) {
         return done(null, result)
